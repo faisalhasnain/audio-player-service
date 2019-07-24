a2ac0ef7c29bbb5d94ed2bba6edd344ae2bf3d91
Faisal Hasnain <faisalhasnain90@gmail.com>
2019-07-24 21:34:19 +0500

    serve static audio file for by filename

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express();
 
 app.use(express.json());
 
+// serve static audio files
+app.use('/audio', express.static('data/songs', { extensions: ['mp3'] }));
+
+// route for song resource
 app.use('/songs', songs);
 
 const { PORT } = process.env;
