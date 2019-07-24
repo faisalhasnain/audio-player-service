a2b6486a107951b71bfc6b566013b594b221232f
Faisal Hasnain <faisalhasnain90@gmail.com>
2019-07-24 21:15:41 +0500

    add pagination for fetching songs list

--- songs.js
+++ songs.js
@@ -4,9 +4,17 @@ const router = require('express').Router();
 const fs = require('fs');
 const { promisify } = require('util');
 
+async function fetchSongs(skip, limit) {
+  const contents = await promisify(fs.readFile)('./data/songs.json');
+  const songs = JSON.parse(contents);
+  const end = skip + limit;
+  return songs.slice(skip, end);
+}
+
 async function find(req, res) {
-  const songs = await promisify(fs.readFile)('./data/songs.json');
-  res.type('json').send(songs);
+  const { skip = '0', limit = '25' } = req.query;
+  const songs = await fetchSongs(Number(skip), Number(limit));
+  res.send(songs);
 };
 
 router.get('/', find);
